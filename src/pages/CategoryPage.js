eaed243aa54a5a2f68c6ef8e43653c28ca3096e9
weslleypmfortunato <111170704+weslleypmfortunato@users.noreply.github.com>
2023-01-09 17:08:42 -0800

    solved table issue in CategoryPage

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -38,14 +38,18 @@ const CategoryPage = () => {
       <table>
         <tbody>
           <tr className="single-category">
-            { filteredRecipes.length > 0 ?
+            { filteredRecipes.length === 0 ? 
+              <div>
+                <p>Working to bring you the most delicious recipes</p>
+                <img src={Loading4} alt="Loading GIF" className="gif" />
+              </div>
+              :         
             filteredRecipes.map(filteredCookie => {
-
                 return (
                   <td key={filteredCookie._id} className="divisor">
-                    <div  className="category-recipes">
+                    <div className="category-recipes">
                       <h5 style={{fontSize: "1rem", marginTop: "10px", textAlign: "center"}} className="category-subtitle"><b>{ filteredCookie.name }</b></h5>
-                      <Link to={`/recipes/${ filteredCookie._id }`}><img src={ filteredCookie.imageUrl } alt="Cookie" className="category-recipes-img"/></Link>
+                      <Link to={`/recipes/${ filteredCookie._id }`}><img src={ filteredCookie.imageUrl } alt="..." className="category-recipes-img"/></Link>
                       <div className="category-servings-preparation">
                         <p className="category-servings-nomargin"><b>Servings:</b> { filteredCookie.servings }</p>
                         <p className="category-preparation-nomargin"><b>Preparation Time:</b> { filteredCookie.prepationTime }</p>
@@ -53,10 +57,7 @@ const CategoryPage = () => {
                     </div>
                   </td>
                 )
-              }) :  <div>
-                      <p>Working to bring you the most delicious recipes</p>
-                      <img src={Loading4} alt="Loading GIF" className="gif" />
-                    </div>
+              }) 
             } 
           </tr>
         </tbody>
